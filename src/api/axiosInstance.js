0b9379ed557e900244a84c84478b1b789961acc8
Rohit <rohit.kushwaha@techstalwarts.com>
2025-10-27 17:23:36 +0530

    Adding code

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const axiosInstance = axios.create({
   
   // baseURL: 'http://localhost:5000/api/', // gateway,
-  baseURL: 'https://vs-sports-backend-1.onrender.com/', // gateway,
+  baseURL: 'https://vs-sports-backend-1.onrender.com/api', // gateway,
 
 
   headers: {
